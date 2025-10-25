e24a4c946bbb29d2884c6e9d64124ee24f8e5fa1
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-25 17:34:31 -0300

    feat: implementa as propriedade da classe Smartphone e faz a classe Iphone herdá-la

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "8888-8888", modelo: "Pro", imei: "9999999", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Dio");
