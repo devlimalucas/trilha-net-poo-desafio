ef97814df748dbaa16837bb94bc4ed5b47947719
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-25 17:39:27 -0300

    feat: implementando a herança na classe Nokia

--- Program.cs
+++ Program.cs
@@ -4,3 +4,10 @@ Console.WriteLine("Smartphone Iphone");
 Smartphone iphone = new Iphone(numero: "8888-8888", modelo: "Pro", imei: "9999999", memoria: 128);
 iphone.Ligar();
 iphone.InstalarAplicativo("Dio");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "7777-7777", modelo: "K", imei: "6666666", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Github");
